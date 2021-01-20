76f1a3d13a4750e6428c14f2280865773547d927
Osmel Contreras <osmeljohan@gmail.com>
2021-01-20 00:43:59 -0500

    Update CarPriceEstimator.java

--- CarPriceEstimator.java
+++ CarPriceEstimator.java
@@ -12,18 +12,6 @@ public class CarPriceEstimator {
 		int ageOfCar = currentYear - yearManufactured + 1;
 		System.out.println("ageOfCar: " +  ageOfCar);
 		
-		
-		
-		
-		// 1. Compute based on yearly depreciation value:
-		//       if age of car is less than or equal to 10 then
-		//               price depreciates by 5% of original sale price every year
-		//               e.g., if original price is 10000, then for 3 year old car
-		//                     price depreciated would be 1500, i.e., sale price would be 8500
-		//       else 
-		//               return salePrice * 0.1 (i.e., 10% of current salePrice)
-		// Note: Use compound arithmetic assignment operators
-		
 		if(ageOfCar <= 10){
 			double depreciationPrice =  salePrice *0.05 * ageOfCar;
 			salePrice -= depreciationPrice; 
@@ -32,20 +20,12 @@ public class CarPriceEstimator {
 			return salePrice * 0.1;
 		}
 		System.out.println("salePrice after depreciation: " + salePrice);
-		
-		
-		// 2. Security Features
-		//      if car does NOT have atleast two airbags AND abs AND ebd
-		//              then reduce price by $1000
+	
 		
 		if(!((airBagsCount >= 2) && (hasAbs == true) && (hasEbd == true))){
 			salePrice -= 1000.0; 
 		}
 		System.out.println("salePrice after accounting for security features: " + salePrice);
-		
-		// 3. Comfort Features
-		// if car has rear-view camera AND either sunroof OR auto AC then
-		//    increment price by $500
 		
 		if(hasRearViewCamera == true && ((hasSunRoof == true) || (hasAutoAC == true))){
 			salePrice += 500.0;
@@ -53,36 +33,22 @@ public class CarPriceEstimator {
 		
 		System.out.println("salePrice after accounting for comfort features: " + salePrice);
 		
-		// 4. Past accidents
-		// if car was involved in an accident then
-		//     reduce price by $500
-		
 		if(hasAccidentHistory == true) {
 			salePrice -= 500.0;
 		}
 		
 		System.out.println("salePrice after accounting for past accidents: " + salePrice);
-		
-		// 5. Based on additional miles driven
+	
 		double expectedMilesDriven = ageOfCar * 10000.0;
 		double additionalMiles = milesDriven - expectedMilesDriven;
 		
-		// a) if # miles over-driven is greater than 1000 AND less than or equal to 10000 then
-		//        reduce sale price by 500
 		if((additionalMiles > 1000) && (additionalMiles <= 10000)){
 			salePrice -= 500.0;
 		}
 		
-		
-		// b) if # miles over-driven is greater than 10000 AND less than or equal to 30000 then
-		//        reduce sale price by 1000
-		
 		if((additionalMiles > 10000) && (additionalMiles <= 30000)){
 			salePrice -= 1000.0;
 		}
-		
-		// c) if # miles over-driven is greater than 30000 then
-		//        reduce sale price by 1500
 		
 		if((additionalMiles > 30000)){
 			salePrice -= 1500.0;
